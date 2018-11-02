05135974caf81693e4bc6eae09bdbe8a01d40f4a
zawnpn <zawnpn@gmail.com>
2018-11-02 22:08:27 +0800

    Deployed f0b90b3 with MkDocs version: 1.0.4

--- assets/extra.js
+++ assets/extra.js
@@ -22,3 +22,12 @@ window.MathJax = {
     messageStyle: "none",
     jax: ["input/TeX","output/SVG"]
 };
+(function(u, c) {
+  var d = document, t = 'script', o = d.createElement(t),
+      s = d.getElementsByTagName(t)[0];
+  o.src = u;
+  if (c) { o.addEventListener('load', function(e) { c(e); }); }
+  s.parentNode.insertBefore(o, s);
+})('//cdn.bootcss.com/pangu/3.3.0/pangu.min.js', function() {
+  pangu.spacingPage();
+});
