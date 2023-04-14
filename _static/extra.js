0d00e9c0ffedbfca437be253addfc13580739de7
zawnpn <zawnpn@gmail.com>
2023-04-14 23:15:31 +0800

    Deployed 54f47d0 with MkDocs version: 1.4.2

--- _static/extra.js
+++ _static/extra.js
@@ -20,3 +20,10 @@ window.MathJax = {
 // document.addEventListener("DOMContentSwitch", function() {
 //   MathJax.typesetPromise();
 // });
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?d919f3c6882717ddb93c07ee58cb658e";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
